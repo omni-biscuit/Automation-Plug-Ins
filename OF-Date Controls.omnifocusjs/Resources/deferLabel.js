fbe524fe039eb7d05b51b4927dd8bec978a0611c
Christian Y <89218720+omni-biscuit@users.noreply.github.com>
2021-09-02 17:40:00 -0500

    Delete OF-Date Controls.omnifocusjs directory

--- OF-Date Controls.omnifocusjs/Resources/deferLabel.js
+++
@@ -1,9 +0,0 @@
-(() => {
-	let action = new PlugIn.Action(() => {
-		// This empty action serves as a label/separator in the menu bar
-	})
-
-	action.validate = () => false;
-	
-	return action;
-})();
--- OF-Date Controls.omnifocusjs/Resources/dueLabel.js
+++
@@ -1,9 +0,0 @@
-(() => {
-	let action = new PlugIn.Action(() => {
-		// This empty action serves as a label/separator when viewing the plug-in actions in the menu bar
-	})
-
-	action.validate = () => false;
-	
-	return action;
-})();
